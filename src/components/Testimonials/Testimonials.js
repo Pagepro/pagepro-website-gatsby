74fa48cb9fc6668338afe5d630b7a08c2b87ab3e
Katarzyna <katarzyna.kotynska@pagepro.pl>
2019-08-26 12:41:34 +0200

    creating Testimonials

---
+++ src/components/Testimonials/Testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {
+  TestimonialsStyled
+} from '../Testimonials/styledComponents'
+
+const Testimonials = () => (
+  <TestimonialsStyled>
+  </TestimonialsStyled>
+)
+
+export default Testimonials
---
+++ src/components/Testimonials/Testimonials.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Testimonials from './Testimonials'
+
+storiesOf('Testimonials', module)
+  .add('default', () => (
+    <Testimonials/>
+  ))
