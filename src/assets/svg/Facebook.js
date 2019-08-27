4e353bfae7e7ba2d6c289fc679f357625ff328bd
Katarzyna <katarzyna.kotynska@pagepro.pl>
2019-08-27 08:33:18 +0200

    corrected name of icons

--- src/assets/svg/Facebook.js
+++ src/assets/svg/Facebook.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const SvgComponent = props => (
+const Facebook = props => (
   <svg width="1em" height="1em" viewBox="0 0 16 16" {...props}>
     <path d="M14.9.2H1.1C.6.2.2.6.2 1.1V15c0 .5.4.9.9.9h7.5V9.8h-2V7.4h2V5.7c0-2 1.2-3.1 3-3.1.9 0 1.6.1 1.8.1v2.1h-1.2c-1 0-1.2.5-1.2 1.1v1.5h2.3L13 9.8h-2v6.1h4c.5 0 .9-.4.9-.9V1c-.1-.4-.5-.8-1-.8z" />
   </svg>
 )
 
-export default SvgComponent
+export default Facebook
