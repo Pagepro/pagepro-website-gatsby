d83f9f450c45c0a2df85c2776770a0bf7cf8ab99
Marek Jakimiuk <marekjakimiuk@Mareks-MBP-2.lan>
2019-09-12 12:38:11 +0200

    Add knobs to StatNumber component.

--- src/components/StatNumber/StatNumber.stories.js
+++ src/components/StatNumber/StatNumber.stories.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
 import StatNumber from './StatNumber'
 
 storiesOf('StatNumber', module)
   .addDecorator(storyFn => <div style={{
      maxWidth: '400px'
   }}>{storyFn()}</div>)
-  .add('default', () => (
-    <StatNumber
-      description="Projects done in 2018"
-      count="176"
-    />
-  ))
+  .add('default', () => {
+    const defaultProps = {
+      description: text('Description', 'Projects done in 2018'),
+      count: text('Count', '176'),
+    }
+    return (
+      <StatNumber
+        { ...defaultProps }
+      />
+    )
+  })
