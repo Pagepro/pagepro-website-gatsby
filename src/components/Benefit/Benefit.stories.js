4a6606c25c8e531cdbf6b8e4218380ccbba6e759
Katarzyna <katarzyna.kotynska@pagepro.pl>
2019-09-20 12:43:05 +0200

    multilines text

--- src/components/Benefit/Benefit.stories.js
+++ src/components/Benefit/Benefit.stories.js
@@ -14,12 +14,12 @@ storiesOf('Benefit', module)
   .add('default', () => {
     const defaultProps = {
       title: text('Title', 'MODULARITY'),
-      description: text('Description', `Building interface with React 
-      components allows to really embrace agile approach to ever-changing
-      business requirements. One component can be seamlessly modified 
-      without breaking the others. Thanks to this, we are able to deliver
-      products that suits needs of our customers without unexpected delays 
-      and errors.`),
+      description: text('Description', 'Building interface with React'
+        + 'components allows to really embrace agile approach to ever-changing'
+        + 'business requirements. One component can be seamlessly modified'
+        + 'without breaking the others. Thanks to this, we are able to deliver'
+        + 'products that suits needs of our customers without unexpected delays'
+        + 'and errors.'),
     }
 
     return (
